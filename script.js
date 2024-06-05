822080ba9f2d6822499702fe9b526abc265cb98e
PatienceNaggayi <98211131+Npatience@users.noreply.github.com>
2024-06-05 15:24:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings:['Web Developer',"App Developer","Software Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
